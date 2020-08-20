1f33aa6d8e72e98907073ac75c914f11fc17e59e
edz007 <eddy.suwandy@gmail.com>
2020-08-20 21:11:23 +0700

    Updates

---
+++ precache-manifest.853fd1d2d0e1b7066664568be58751b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6531faa955eeb887c961542d63eea55c",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "f653fa2cdb9bd3b4ac97",
+    "url": "/roboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5b979e6148ceb544be32",
+    "url": "/roboFriends/static/css/main.1c44c36f.chunk.css"
+  },
+  {
+    "revision": "f653fa2cdb9bd3b4ac97",
+    "url": "/roboFriends/static/js/2.6f73cbf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriends/static/js/2.6f73cbf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b979e6148ceb544be32",
+    "url": "/roboFriends/static/js/main.a5c9a823.chunk.js"
+  },
+  {
+    "revision": "95f6d784996b28121a5a",
+    "url": "/roboFriends/static/js/runtime-main.867902ca.js"
+  }
+]);
